7d64b5474ad91c4ced4e85b29b2334ba93d8d587
Mark Kuharich <kuharich@hotmail.com>
2020-01-09 00:24:49 -0800

    Initial Commit

---
+++ Spell-checker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Spell-checker
+//
+//  Created by Mark Kuharich on 1/9/20.
+//  Copyright © 2020 Mark Kuharich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
